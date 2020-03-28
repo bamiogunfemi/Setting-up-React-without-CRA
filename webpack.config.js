0d66d79bda48e676dab936eda4ad5fd26a32e7e9
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-03-28 10:44:20 +0100

    React app setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundlefile.js'
+  },
+module: {
+rules : [
+         {
+          test: /\.js$/,
+          use: [ 'babel-loader'],
+          exclude: /node_modules/,
+         }
+        ]
+},
+  plugins: [
+    new HtmlWebpackPlugin({
+       template: './src/index.html'
+    })
+  ]
+}   
